a7a8474896382f85014a2e52634c1ec2a225ee17
geekplux <geekplux@qq.com>
2016-08-02 11:32:13 +0800

    make js standard and hide page article-meta

--- source/js/typing.js
+++ source/js/typing.js
@@ -1,24 +1,26 @@
-(function($){
-
+(function ($) {
   // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
+  $('.article-entry').each(function (i) {
+    $(this).find('img').each(function () {
+      if ($(this).parent().hasClass('fancybox')) return
 
-      var alt = this.alt;
+      var alt = this.alt
 
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+      if (alt) {
+        $(this).after('<span class="caption">' + alt +
+                      '</span>')
+      }
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-    });
+      $(this).wrap('<a href="' + this.src + '" title="' + alt +
+                   '" class="fancybox"></a>')
+    })
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
+    $(this).find('.fancybox').each(function () {
+      $(this).attr('rel', 'article' + i)
+    })
+  })
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
+  if ($.fancybox) {
+    $('.fancybox').fancybox()
   }
-
-})(jQuery);
+})(jQuery)
