1761e605f3b6d5bf317a645c1f7d73c9b93f60da
geekplux <geekplux@qq.com>
2017-12-01 18:27:38 +0800

    add donate section

--- source/js/typing.js
+++ source/js/typing.js
@@ -7,12 +7,10 @@
       var alt = this.alt
 
       if (alt) {
-        $(this).after('<span class="caption">' + alt +
-                      '</span>')
+        $(this).after('<span class="caption">' + alt + '</span>')
       }
 
-      $(this).wrap('<a href="' + this.src + '" title="' + alt +
-                   '" class="fancybox"></a>')
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>')
     })
 
     $(this).find('.fancybox').each(function () {
@@ -23,4 +21,45 @@
   if ($.fancybox) {
     $('.fancybox').fancybox()
   }
+
+
+  $(document).ready(function() {
+	  var QRBox	=	$('#QRBox');
+	  var MainBox	=	$('.MainBox');
+	  var BTCQR	=	$('#BTCQR');
+	  var AliPayQR	=	$('#AliPayQR');
+	  var WeChatQR	=	$('#WeChatQR');
+    var currentQR;
+
+	  function showQR(QR) {
+		  $('#DonateText,#donateBox,#github').addClass('blur');
+      currentQR = QR;
+		  QRBox.fadeIn(300,function(argument) {
+			  QR.addClass('showQR');
+		  });
+	  }
+
+	  $('#donateBox>li').click(function(event) {
+		  var thisID	=	$(this).attr('id');
+		  if (thisID === 'BTC') {
+			  showQR(BTCQR);
+			  new Clipboard('#BTCBn');
+		  } else if (thisID === 'AliPay') {
+			  showQR(AliPayQR);
+		  } else if (thisID === 'WeChat') {
+			  showQR(WeChatQR);
+		  }
+	  });
+
+	  MainBox.click(function(event) {
+		  if (currentQR) currentQR.removeClass('showQR').addClass('hideQR');
+		  setTimeout (function(a) {
+			  QRBox.fadeOut(300,function(argument) {
+				  MainBox.removeClass('hideQR');
+			  });
+			  $('#DonateText,#donateBox,#github').removeClass('blur');
+		  },600);
+
+	  });
+  });
 })(jQuery)
